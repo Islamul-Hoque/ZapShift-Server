7054ef47e5d034073317bc7af5fe899060f09a9e
Islamul-Hoque <islamulhoque2006@gmail.com>
2025-11-29 21:19:10 +0600

    Initialize project with basic setup

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express')
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port = process.env.PORT || 3000
+
+// middleware
+app.use(express.json());
+app.use(cors());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xue6gdd.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // mongoDB collections
+        const db = client.db('Zap_Shift_User');
+        const parcelsCollection = db.collection('parcels');
+
+        // parcel api
+        app.get('/parcels', async (req, res) => {
+            const query = {}
+            const {email} = req.query;
+
+            if(email){
+                query.senderEmail = email;
+            }
+
+            const cursor = parcelsCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        app.post('/parcels', async (req, res) => {
+            const parcel = req.body;
+            const result = await parcelsCollection.insertOne(parcel);
+            res.send(result)
+        })
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+
+    }
+}
+run().catch(console.dir);
+
+app.get('/', (req, res) => {
+    res.send('zap is shifting shifting!')
+})
+
+app.listen(port, () => {
+    console.log(`ZapShip listening on port ${port}`)
+})
